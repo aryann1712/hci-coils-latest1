bb1628ae48e65c52ec6645e69cc70880287b5365
rounak <rounak>
2025-02-16 13:00:59 +0530

    work on the cart page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { MdOutlineMailOutline } from "react-icons/md";
-
+import { MdOutlineShoppingCart } from "react-icons/md";
 import React from "react";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
@@ -37,7 +37,8 @@ const Navbar: React.FC<NavbarProps> = ({ user }) => {
           <h3>{process.env.NEXT_PUBLIC_CLIENT_EMAIL}</h3>
         </a>
 
-        {/* social and login */}
+
+        {/* card and order and social and login */}
         <div className="flex gap-3 text-gray-500 items-center">
           <a
             href={process.env.NEXT_PUBLIC_FACEBOOK_LINK}
@@ -71,6 +72,18 @@ const Navbar: React.FC<NavbarProps> = ({ user }) => {
           >
             <FaLinkedinIn className="h-7 w-7  cursor-pointer hover:text-white hover:bg-blue-500 rounded-full p-[2px]" />
           </a>
+
+          <div className=" ml-5">
+            <Link href={"/orders"}><h4 className="font-semibold hover:text-red-500">Orders</h4></Link>
+          </div>
+
+
+          <div className="relative mx-2">
+            <Link href={"/cart"}>
+              <MdOutlineShoppingCart className="font-bold text-[22px] cursor-pointer hover:text-red-500 relative" />
+              <p className="absolute -bottom-3 -right-3 rounded-full text-sm  text-center font-semibold text-white bg-red-500 h-5 px-[4px] pb-[10px]">1</p>
+            </Link>
+          </div>
 
           <div className="ml-2 px-4 py-2 bg-blue-700 text-white rounded-md text-sm font-semibold  cursor-pointer">
             <Link href={"/auth/signin"}><h4>Login</h4></Link>
